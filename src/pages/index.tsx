abf163406b79e8dbd8d66930bb98deea7876aeb1
Jorge de Souza <jorgejrdj1@gmail.com>
2021-06-08 22:05:59 -0300

    timeString - até agora está funcionando

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { api } from '../services/api';
 
 type Episode = {
   id: string;
   title: string;
   members: string;
+  published_at: string;
   //... 
 }
 
@@ -21,8 +25,26 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
 
   return {
     props: {

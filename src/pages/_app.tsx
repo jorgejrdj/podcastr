2c2e2e13bda13a460b6a3e622c828c1e4125c1a7
Jorge de Souza <jorgejrdj1@gmail.com>
2021-06-20 23:03:01 -0300

    primeira parte - context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,21 @@ import '../styles/global.scss';
 
 import styles from '../styles/app.module.scss';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../context/PlayerContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
+    <PlayerContext.Provider value={'Diego'}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
 
-      </main>
-      <Player />
+        </main>
+        <Player />
 
-    </div>
+      </div>
+    </PlayerContext.Provider>
   )
 }
 

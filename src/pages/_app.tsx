3a79b17f7ba738f3a764a9c5f486e0ffdf859af6
Jorge de Souza <jorgejrdj1@gmail.com>
2021-06-22 21:48:28 -0300

    playercontext ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,30 +3,12 @@ import '../styles/global.scss';
 
 import styles from '../styles/app.module.scss';
 import { Player } from '../components/Player';
-import { PlayerContext } from '../context/PlayerContext';
-import { useState } from 'react';
+
+import { PlayerContextProvider } from '../context/PlayerContext'
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state)
-  }
-
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -36,7 +18,8 @@ function MyApp({ Component, pageProps }) {
         <Player />
 
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
+
   )
 }
 
